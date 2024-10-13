413c5f45fff89f691acefe10203ba24d20151ec8
boboiboyss <boysimbolon5@gmail.com>
2024-10-13 11:57:14 +0700

    fix hooks toptup

--- src/features/topup/hooks/useTopup.ts
+++ src/features/topup/hooks/useTopup.ts
@@ -1,16 +1,8 @@
-import { z } from "zod";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "@tanstack/react-form";
 import toast from "react-hot-toast";
 import axios from "axios";
 import { ToupupDto } from "../dto/TopupDTO";
-
-const registerSchema = z.object({
-  top_up_amount: z
-    .number()
-    .min(10000, "Top up amount minimum is 10.000")
-    .max(1000000, "Top up amount maximum is 1.000.000"),
-});
 
 export default function useTopup() {
   const queryClient = useQueryClient();
@@ -29,21 +21,21 @@ export default function useTopup() {
         );
         return res.data;
       } catch (error) {
-        console.error("Login error:", error); // Log the error for debugging
-        throw error; // Re-throw the error to trigger onError
+        console.error("Top-up error:", error);
+        throw error;
       }
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["balance"] });
-      toast.success("User topup successfully");
+      toast.success("User top-up successfully");
     },
     onError: (error) => {
       if (axios.isAxiosError(error)) {
-        console.error("Axios error response:", error.response?.data); // Log response error
+        console.error("Axios error response:", error.response?.data);
       } else {
         console.error("Unexpected error:", error);
       }
-      toast.error("User topup failed!");
+      toast.error("User top-up failed!");
     },
   });
 
@@ -52,14 +44,12 @@ export default function useTopup() {
       top_up_amount: 0,
     },
     onSubmit: async (values) => {
-      form.reset();
-      await mutateAsync(registerSchema.safeParse(values.value));
+      const topUpData: ToupupDto = {
+        data: { top_up_amount: +values.value.top_up_amount },
+      };
 
-      const result = registerSchema.safeParse(values.value);
-      if (!result.success) {
-        toast.error(result.error.errors[0].message);
-        return;
-      }
+      await mutateAsync(topUpData);
+      form.reset();
     },
   });
 
